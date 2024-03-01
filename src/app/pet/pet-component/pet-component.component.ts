fb64509f9d285dc5b9e7ab33777546e0d28a8967
Kevin Gomes <150367749+KevGomezz@users.noreply.github.com>
2024-03-01 15:42:33 -0300

    subindo

--- src/app/pet/pet-component/pet-component.component.ts
+++ src/app/pet/pet-component/pet-component.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { PetService } from '../services/pet.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+interface Animal {
+  id: number;
+  nome: string;
+  raca: string;
+  adotado: boolean;
+}
+
 @Component({
   selector: 'app-pet-component',
   templateUrl: './pet-component.component.html',
@@ -10,7 +17,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class PetComponentComponent implements OnInit {
   form!: FormGroup;
-  animais: any[] = [];
+  animais: Animal[] = [];
+  animaisAdotados: Animal[] = [];
+  mostrarListaAdotados = false;
+  animaisParaAdotar: number[] = [];
 
   constructor(private petService: PetService) {}
 
@@ -26,14 +36,14 @@ export class PetComponentComponent implements OnInit {
 
   cadastrarAnimal() {
     if (this.form.valid) {
-      const novoAnimal: any = {
+      const novoAnimal: Animal = {
         id: this.form.value.id,
         nome: this.form.value.nome,
         raca: this.form.value.raca,
         adotado: false,
       };
 
-      this.petService.cadastrarAnimal(novoAnimal).subscribe((response: any) => {
+      this.petService.cadastrarAnimal(novoAnimal).subscribe((response: Animal) => {
         console.log('Animal cadastrado:', response);
         this.limparCampos();
         this.listarTodosAnimais();
@@ -43,14 +53,14 @@ export class PetComponentComponent implements OnInit {
 
   atualizarAnimal() {
     if (this.form.valid) {
-      const animalAtualizado: any = {
+      const animalAtualizado: Animal = {
         id: this.form.value.id,
-        // Adicione mais campos conforme necessário
-        // nome: this.form.value.nome,
-        // raca: this.form.value.raca,
+        nome: this.form.value.nome,
+        raca: this.form.value.raca,
+        adotado: false,
       };
 
-      this.petService.atualizarAnimal(animalAtualizado).subscribe((response: any) => {
+      this.petService.atualizarAnimal(animalAtualizado).subscribe((response: Animal) => {
         console.log('Animal atualizado:', response);
         this.limparCampos();
         this.listarTodosAnimais();
@@ -60,31 +70,69 @@ export class PetComponentComponent implements OnInit {
 
   excluirAnimal() {
     if (this.form.valid) {
-      const idAnimalExcluir: any = {
-        id: this.form.value.id,
-      };
+      const idAnimalExcluir: number = this.form.value.id;
 
       this.petService.excluirAnimal(idAnimalExcluir).subscribe((response: any) => {
         console.log('Animal excluído:', response);
+
+        // Remove o animal da lista de animais adotados, se existir
+        this.animaisAdotados = this.animaisAdotados.filter(animal => animal.id !== idAnimalExcluir);
+
+        // Remove o ID do animal da lista 'animaisParaAdotar'
+        this.animaisParaAdotar = this.animaisParaAdotar.filter(id => id !== idAnimalExcluir);
+
         this.limparCampos();
         this.listarTodosAnimais();
       });
     }
   }
 
-  adotarAnimal() {
-    // Implemente conforme necessário, usando o serviço petService
-    // Você pode obter o ID do animal a ser adotado do formulário e enviar uma solicitação para adotar no servidor
+  adotarAnimal(id: number) {
+    const animalParaAdotar = this.animais.find(animal => animal.id === id);
+
+    if (animalParaAdotar && !this.animaisParaAdotar.includes(id)) {
+      this.petService.adotarAnimal(id).subscribe(
+        (response: Animal) => {
+          console.log('Animal adotado:', response);
+          const animalIndex = this.animais.findIndex(animal => animal.id === id);
+
+          if (animalIndex !== -1) {
+            // Remova o animal da lista 'animais'
+            this.animais.splice(animalIndex, 1);
+
+            // Adicione o animal à lista 'animaisAdotados'
+            this.animaisAdotados.push(response);
+
+            // Adicione o ID do animal à lista 'animaisParaAdotar'
+            this.animaisParaAdotar.push(id);
+          } else {
+            console.warn('Tentativa de adotar um animal que não existe na lista.');
+          }
+        },
+        (error: any) => {
+          console.error('Erro ao adotar animal:', error);
+          // Adicione lógica para lidar com o erro conforme necessário.
+        }
+      );
+    } else {
+      console.warn('Tentativa de adotar um animal que não existe na lista ou já foi adotado.');
+    }
   }
 
   listarAnimaisAdotados() {
-    this.petService.listarAnimaisAdotados().subscribe(response => {
-      this.animais = response;
+    this.petService.listarAnimaisAdotados().subscribe((response: Animal[]) => {
+      this.animaisAdotados = response;
+      this.mostrarListaAdotados = true;
     });
   }
 
+  voltarParaAnimaisDisponiveis() {
+    this.mostrarListaAdotados = false;
+    this.animaisAdotados = [];
+  }
+
   private listarTodosAnimais() {
-    this.petService.listarAnimais().subscribe(response => {
+    this.petService.listarAnimais().subscribe((response: Animal[]) => {
       this.animais = response;
     });
   }
--- src/app/pet/services/pet.service.ts
+++ src/app/pet/services/pet.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -10,12 +9,11 @@ export class PetService {
 
   private readonly API = "https://localhost:7152";
 
-  constructor(
-    private http:HttpClient,) { }
+  constructor(private http: HttpClient) { }
 
   cadastrarAnimal(novoAnimal: any): Observable<any> {
-    const cadastrarUrl = `${this.API}/Cadastrar-animal`;
-    return this.http.get<any>(cadastrarUrl, novoAnimal);
+    const cadastrarUrl = `${this.API}/Animal/Cadastrar-animal`;
+    return this.http.post<any>(cadastrarUrl, novoAnimal);
   }
 
   listarAnimais(): Observable<any[]> {
@@ -24,22 +22,22 @@ export class PetService {
   }
 
   listarAnimaisAdotados(): Observable<any[]> {
-    const adotadosUrl = `${this.API}/Listar-animais-adotados`;
+    const adotadosUrl = `${this.API}/Animal/ListarAdotados`;
     return this.http.get<any[]>(adotadosUrl);
   }
 
   atualizarAnimal(animal: any): Observable<any> {
-    const atualizarUrl = `${this.API}/Atualizar-animal/${animal.id}`;
+    const atualizarUrl = `${this.API}/Animal${animal.id}`;
     return this.http.put<any>(atualizarUrl, animal);
   }
 
   excluirAnimal(id: number): Observable<any> {
-    const excluirUrl = `${this.API}/Excluir-animal/${id}`;
+    const excluirUrl = `${this.API}/Animal/${id}`;
     return this.http.delete<any>(excluirUrl);
   }
 
   adotarAnimal(id: number): Observable<any> {
-    const adotarUrl = `${this.API}/Adotar-animal/${id}`;
-    return this.http.put<any>(adotarUrl, null);
+    const adotarUrl = `${this.API}/Animal/AdotarAnimal${id}`;
+    return this.http.patch<any>(adotarUrl, null);
   }
 }
